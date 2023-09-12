19022fa92b4584cae18881fdc9eb462512bef6f0
said4shi <squlizade1@std.beu.edu.az>
2023-09-13 03:33:27 +0400

    gyuhuj

--- OopIntro/src/Main.java
+++ OopIntro/src/Main.java
@@ -1,6 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        Product[] products = {};
+
+        Product product1 = new Product(1,"Lenovo V14",15000,"16 GB RAM"); // to create reference, instance
+
+        Product product2 = new Product();
+        product2.id = 2;
+        product2.name = "Lenovo V15";
+        product2.unitPrice = 20000;
+        product2.detail = "32 GB RAM";
+
+        Product product3 = new Product();
+        product3.id = 3;
+        product3.name = "HP 5";
+        product3.unitPrice = 25000;
+        product3.detail = "8 GB RAM";
+
+
+        Product[] products = {product1,product2,product3};
+
+        for (Product product : products){
+            System.out.println(product.name);
+
+        }
+
+        Category category1 = new Category();
+        category1.id = 1;
+        category1.name = "Computer";
+
+        Category category2 = new Category();
+        category2.id = 1;
+        category2.name = "Home/Garden";
+
+        Category[] categories = {category1,category2};
+
+        for (Category category : categories){
+            System.out.println(category.name);
+        }
 
 
     }
--- OopIntro/src/Product.java
+++ OopIntro/src/Product.java
@@ -1,4 +1,18 @@
 public class Product {
+
+    Product() {
+        System.out.println("ready");
+    }
+
+    Product(int id, String name, double unitPrice, String detail) {
+        this(); // if you call Product() without parameters
+        this.id = id;
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.detail = detail;
+
+    }
+
     int id;
     String name;
     double unitPrice;
